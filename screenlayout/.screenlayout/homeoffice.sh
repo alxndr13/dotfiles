ff0402c24e50d4de5917b36ebdbe2741153a132f
Alexander Huck <alxndr13@mailbox.org>
2020-04-13 22:58:56 +0200

    fix: reset wallpaper after switching

--- screenlayout/.screenlayout/homeoffice.sh
+++ screenlayout/.screenlayout/homeoffice.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 xrandr --output eDP-1 --off --output HDMI-1 --off --output DP-1 --mode 2560x1440 --rate 143.91 --pos 1920x0 --rotate normal --output HDMI-2 --off
+sleep 1
+nitrogren --restore
