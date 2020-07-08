74e19b3b2ad2c976b39c14bbdd4be54aba98a350
Alexander Huck <alxndr13@mailbox.org>
2020-07-08 19:10:30 +0200

    fix: kill pulseaudio when switching

--- screenlayout/.screenlayout/homeoffice.sh
+++ screenlayout/.screenlayout/homeoffice.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 xrandr --output eDP-1 --off --output HDMI-1 --off --output DP-1 --mode 2560x1440 --rate 143.91 --pos 1920x0 --rotate normal --output HDMI-2 --off
 sleep 1
-nitrogren --restore
+nitrogen --set-zoom-fill /usr/share/backgrounds/sven-scheuermeier-VNseEaTt9w4-unsplash.jpg
+killall conky; start_conky_maia
+pulseaudio -k
