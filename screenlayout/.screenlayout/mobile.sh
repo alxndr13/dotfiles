d1bafb73bc58711fad68c2bccca3bd63f31711cb
Alexander Huck <alxndr13@mailbox.org>
2020-12-29 19:52:10 +0100

    fix: new device, new settings

--- screenlayout/.screenlayout/mobile.sh
+++ screenlayout/.screenlayout/mobile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off
+xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-1 --off
 sleep 1
-nitrogen --set-zoom-fill /usr/share/backgrounds/sven-scheuermeier-VNseEaTt9w4-unsplash.jpg
+nitrogen --set-zoom-fill /usr/share/backgrounds/12-CANYON-Wallpaper-2400x1350-Dude.jpg
 killall conky; start_conky_maia
