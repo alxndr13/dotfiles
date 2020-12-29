3428e0ea727c7983c95df19333726dfef8e8deae
Alexander Huck <alxndr13@mailbox.org>
2020-12-29 18:22:13 +0100

    fix: new laptop, new modes

--- screenlayout/.screenlayout/homeoffice.sh
+++ screenlayout/.screenlayout/homeoffice.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output HDMI-1 --off --output HDMI-1 --mode 2560x1440 --rate 143.91 --pos 1920x0 --rotate normal --output HDMI-2 --off
+xrandr --output eDP --off --output HDMI-A-0 --off --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 2560x1440 --rate 143.91  --pos 0x0 --rotate normal
 sleep 1
-nitrogen --set-zoom-fill /usr/share/backgrounds/sven-scheuermeier-VNseEaTt9w4-unsplash.jpg
+nitrogen --set-zoom-fill /usr/share/backgrounds/12-CANYON-Wallpaper-2400x1350-Dude.jpg
 killall conky; start_conky_maia
-pulseaudio -k
-sleep 2
-pacmd set-default-sink 7
