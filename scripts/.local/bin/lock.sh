c77706226dfbb3d03badb8d499239c1f1bd76b3d
Alexander Huck <alxndr13@mailbox.org>
2021-08-01 15:52:15 +0200

    updated

---
+++ scripts/.local/bin/lock.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+swaylock --hide-keyboard-layout \
+    --indicator-caps-lock \
+    --clock \
+    --text-color D8DEE9 \
+    --ring-color 2E3440 \
+    --ring-clear-color EBCB8B \
+    --ring-wrong-color BF616A \
+    --text-ver-color 5E81AC \
+    --indicator-radius 110 \
+    --indicator-thickness 8 \
+    --inside-color 4C566A \
+    --inside-clear-color 4C566A \
+    --key-hl-color ECEFF4 \
+    --grace 3 \
+    -f \
+    --fade-in 1
