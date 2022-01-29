6410d260a53c34faf44d0ce6d9ebc0b93eeebfdd
Alexander Huck <alxndr13@mailbox.org>
2022-01-29 13:38:05 +0100

    fix: internal cam comment

--- scripts/.local/bin/cam-toggle.sh
+++ scripts/.local/bin/cam-toggle.sh
@@ -2,8 +2,8 @@
 sudo stat /sys/bus/usb/drivers/usb/2-2
 if [[ $? -eq 0 ]]; then
     echo "2-2" | sudo tee /sys/bus/usb/drivers/usb/unbind
-    notify-send "Webcam Toggle" "Toggled Cam to [off]" -i microphone-disabled-symbolic.symbolic -t 2000
+    notify-send "Webcam Toggle" "Toggled Internal Cam to [off]" -i microphone-disabled-symbolic.symbolic -t 2000
 else
     echo "2-2" | sudo tee /sys/bus/usb/drivers/usb/bind
-    notify-send "Webcam Toggle" "Toggled Cam to [on]" -i microphone-disabled-symbolic.symbolic -t 2000
+    notify-send "Webcam Toggle" "Toggled Internal Cam to [on]" -i microphone-disabled-symbolic.symbolic -t 2000
 fi
