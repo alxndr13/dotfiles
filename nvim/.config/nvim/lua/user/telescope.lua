5ff420209c5b850b382ff6398050e208de0cff8f
Alexander Huck <alxndr13@mailbox.org>
2022-08-06 17:27:48 +0200

    feat: use new nvim config based on the lunarvim ide base

---
+++ nvim/.config/nvim/lua/user/telescope.lua
@@ -0,0 +1,23 @@
+local status_ok, telescope = pcall(require, "telescope")
+if not status_ok then
+  return
+end
+
+local actions = require "telescope.actions"
+
+telescope.setup {
+  defaults = {
+
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "smart" },
+    file_ignore_patterns = { ".git/", "node_modules" },
+
+    mappings = {
+      i = {
+        ["<Down>"] = actions.move_selection_next,
+        ["<Up>"] = actions.move_selection_previous,
+      },
+    },
+  },
+}
