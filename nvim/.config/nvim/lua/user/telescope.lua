0d909ed625f37b88c52dacb948bbb5396dff5610
Alexander Huck <alxndr13@mailbox.org>
2022-08-07 18:42:00 +0200

    fix: update telescope config

--- nvim/.config/nvim/lua/user/telescope.lua
+++ nvim/.config/nvim/lua/user/telescope.lua
@@ -7,11 +7,12 @@ local actions = require "telescope.actions"
 
 telescope.setup {
   defaults = {
-
     prompt_prefix = " ",
     selection_caret = " ",
-    path_display = { "smart" },
+    path_display = { shorten = 5 },
     file_ignore_patterns = { ".git/", "node_modules" },
+    selection_strategy = "reset",
+    sorting_strategy = "descending",
 
     mappings = {
       i = {
@@ -19,5 +20,26 @@ telescope.setup {
         ["<Up>"] = actions.move_selection_previous,
       },
     },
+  },
+  pickers = {
+  },
+  extensions = {
+      fzf = {
+        fuzzy = true, -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+      },
+  },
+  vimgrep_arguments = {
+    "rg",
+    "--color=never",
+    "--no-heading",
+    "--with-filename",
+    "--line-number",
+    "--column",
+    "--smart-case",
+    "--hidden",
+    "--glob=!.git/",
   },
 }
