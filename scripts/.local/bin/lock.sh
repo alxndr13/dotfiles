457640caf48194183bf9d4e18d00ad86efd5b066
Alexander Huck <alxndr13@mailbox.org>
2022-12-10 12:58:19 +0100

    feat: update lock

--- scripts/.local/bin/lock.sh
+++ scripts/.local/bin/lock.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
+export XDG_RUNTIME_DIR=/run/user/1000
+export XDG_CURRENT_DESKTOP="sway"
+export WAYLAND_DISPLAY="wayland-1"
+export XDG_SESSION_TYPE=wayland
+export XDG_SESSION_CLASS=user
+export XDG_SESSION_ID=1
+
 swaylock --hide-keyboard-layout \
     --indicator-caps-lock \
     --clock \
-    --text-color D8DEE9 \
-    --ring-color 2E3440 \
-    --ring-clear-color EBCB8B \
-    --ring-wrong-color BF616A \
-    --text-ver-color 5E81AC \
+    --text-color c6d0f5 \
+    --text-clear-color c6d0f5 \
+    --text-ver-color c6d0f5 \
+    --text-wrong-color 4C566A \
+    --ring-color 303446 \
+    --ring-clear-color ef9f76 \
+    --ring-wrong-color 4C566A \
+    --ring-ver-color a5adce \
     --indicator-radius 110 \
     --indicator-thickness 8 \
     --inside-color 4C566A \
     --inside-clear-color 4C566A \
-    --key-hl-color ECEFF4 \
-    --grace 3 \
+    --inside-ver-color 4C566A \
+    --inside-wrong-color e78284 \
+    --key-hl-color babbf1 \
+    -n \
+    --grace 1 \
     -f \
-    --fade-in 1
+    -e \
+    -i /home/hucka/.wallpaper/red_peach.png \
+    -s fill \
+    -l \
+    --fade-in 2
